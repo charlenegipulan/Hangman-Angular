c13007018fa4d66624763915cc095e6f315bf9a0
Charlene Gipulan <charlenegipulan@gmail.com>
2020-03-16 22:29:19 -0700

    create new Game function to initialize game

--- app.js
+++ app.js
@@ -12,7 +12,20 @@ $scope.input = {
 } 
 
 var selectRandomWord = function() {
-    var index = Math.round(Math.random() * words.length)
+    var index = Math.round(Math.random() * words.length);
+    return words[index]
 }
+
+var newGame = function() {
+    $scope.incorrectLettersChosen = [];
+    $scope.incorrectLettersChosen = [];
+    $scope.correctLettersChosen = [];
+    $scope.guesses  = 6;
+    $scope.displayWord = '';
+    selectedWord = selectRandomWord();
+    console.log(selectedWord);
+}
+
+newGame();
 
 }]);
