98ced23975cc8de4af000dbe8ea62ec0a225277a
Charlene Gipulan <charlenegipulan@gmail.com>
2020-03-16 23:32:54 -0700

    create choose letter function

--- app.js
+++ app.js
@@ -18,7 +18,6 @@ var selectRandomWord = function() {
 
 var newGame = function() {
     $scope.incorrectLettersChosen = [];
-    $scope.incorrectLettersChosen = [];
     $scope.correctLettersChosen = [];
     $scope.guesses  = 6;
     $scope.displayWord = '';
@@ -33,11 +32,39 @@ var newGame = function() {
     console.log(tempDisplayWord);
     $scope.displayWord = tempDisplayWord;
 
-
 }
 
 $scope.letterChosen = function() {
-    
+
+    for (var i = 0; i < $scope.correctLettersChosen.length; i++) {
+        if($scope.correctLettersChosen[i].toLowerCase() == $scope.input.letter.toLowerCase()){
+            $scope.input.letter == "";
+            return;
+        }
+
+    }
+
+    for (var i = 0; i < $scope.incorrectLettersChosen.length; i++) {
+        if($scope.incorrectLettersChosen[i].toLowerCase() == $scope.input.letter.toLowerCase()){
+            $scope.input.letter == "";
+            return;
+        }
+    }
+
+    var correct = false;
+    for (var i = 0; i < selectedWord.length; i++) {
+        if(selectedWord[i].toLowerCase() == $scope.input.letter.toLowerCase()) {
+            $scope.displayWord = $scope.displayWord.slice(0,i)+$scope.input.letter.toLowerCase()+$scope.displayWord.slice(i+1);
+            correct = true;
+        }
+    }
+
+    if(correct){
+        $scope.correctLettersChosen.push($scope.input.letter.toLowerCase());
+    } else {
+        $scope.incorrectLettersChosen.push($scope.input.letter.toLowerCase());
+    }
+    $scope.input.letter = "";
 
 }
 
